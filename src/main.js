94ed8c46d778b66483939a0af4b4c21edbb799fd
Lucas Saliés Brum <lucas@archlinux.com.br>
2021-03-02 17:59:06 -0400

    src/main.js alterado para incluir os arquivos do Twitter Bootstrap 5

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Arquivo do Twitter Bootstrap 5 (JS e SCSS)
+import 'bootstrap'
+import './assets/scss/bootstrap.scss'
+
 createApp(App).use(router).mount('#app')
